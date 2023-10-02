37f56f7f3fe9a57721504a7fb329e6bdbabd97eb
Rafael Ayala <rafayala30@gmail.com>
2023-10-01 20:09:21 -0400

    created app.js and modified html bootstrap components

---
+++ src/App.js
@@ -0,0 +1,32 @@
+const API_BASE = 'http://localhost:8080/api/cruises';
+let cardData = {};
+
+// Add event listeners for page
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Save button will be in Trip Package Card
+    document.getElementById('add_btn').addEventListener('click', addPackage);
+    document.getElementById('delete_btn').addEventListener('click', removePackage);
+    document.getElementById('summary_btn').addEventListener('click', generateTripSummary)
+
+})
+
+
+function addPackage() {
+
+}
+
+
+function removePackage() {
+
+}
+
+
+function generateTripSummary() {
+
+}
+
+
+function calculateTotalTripTime() {
+
+}
