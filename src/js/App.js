44ff985bd90aad4c49feffcc44b37ce0ba38b353
Rafael Ayala <rafayala30@gmail.com>
2023-10-03 17:19:44 -0400

    modified carousel in html and js

--- src/js/App.js
+++ src/js/App.js
@@ -20,8 +20,56 @@ document.addEventListener('DOMContentLoaded', () => {
 
 async function populateCarouselImage() {
     const sites = await getSites();
+
+    sites.forEach((eachSite, index) => {
+
+        const name = eachSite.site;
+        const image = eachSite.imgUrl;
+        const planet = eachSite.site_planet;
+        const fact = eachSite.site_fact;
+
+        const carouselParent = document.createElement('div');
+        carouselParent.setAttribute('class', 'carousel-item c-item');
+
+        const carouselImg = document.createElement('img');
+        carouselImg.setAttribute('src', image);
+        carouselImg.setAttribute('class', 'd-block w-100 c-img');
+
+        const carouselCaptionParent = document.createElement('div');
+        carouselCaptionParent.setAttribute('class', 'carousel-caption d-none d-md-block');
+
+        const carouselName = document.createElement('h5');
+        carouselName.innerText = name;
+
+        const carouselPlanet = document.createElement('h4');
+        carouselPlanet.innerText = planet;
+
+        const carouselFact = document.createElement('p');
+        carouselFact.innerText = fact;
+
+
+        carouselParent.insertAdjacentElement('beforeend', carouselImg);
+        carouselParent.insertAdjacentElement('beforeend', carouselCaptionParent);
+
+        carouselCaptionParent.insertAdjacentElement('beforeend', carouselName);
+        carouselCaptionParent.insertAdjacentElement('beforeend', carouselPlanet);
+        carouselCaptionParent.insertAdjacentElement('beforeend', carouselFact);
+
+        document.querySelector('div.carousel-inner')
+                .insertAdjacentElement('beforeend', carouselParent);
+
+
+        // Assign button
+        const carouselButton = document.createElement('button');
+        carouselButton.setAttribute('data-bs-target', '#hero-carousel');
+        carouselButton.setAttribute('data-bs-slide-to', index+1);
+
+        document.querySelector('div.carousel-indicators')
+                .insertAdjacentElement('beforeend', carouselButton);
+
+    });
+
     console.log(sites);
-    
 }
 
 
