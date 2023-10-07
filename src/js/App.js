91effb4b3bf9d7417a8ea65326d3ff88ae0ecbac
Rafael Ayala <rafayala30@gmail.com>
2023-10-07 00:16:58 -0400

    modified images and layout. modified JS files for event listeners on modals

--- src/js/App.js
+++ src/js/App.js
@@ -1,17 +1,49 @@
 import {getSites} from "./PlanetService.js";
+import { addToItinerary, removeItemFromItinerary, displayItinerary} from "./ItineraryService.js";
 
-
-let cardData = {};
 
 
 
 // Add event listeners for page
 document.addEventListener('DOMContentLoaded', () => {
     // Save button will be in Trip Package Card
-    // document.getElementById('add_btn').addEventListener('click', addPackage);
-    // document.getElementById('delete_btn').addEventListener('click', removePackage);
-    // document.getElementById('summary_btn').addEventListener('click', generateTripSummary);
-    // document.getElementById('view-itin-btn').addEventListener('click', displayItinerary);
+    document.querySelector('.save-itin-btn').addEventListener('click', (event) => {
+        // Find parent element
+        const modalContent = event.target.closest('.modal-content');
+
+        if (modalContent) {
+            // Extract title and image source from modalContent
+            const title = modalContent.querySelector('.package-label').innerText;
+            const imageSrc = modalContent.querySelector('.banner-img').src;
+
+            // Call addToItinerary function
+            addToItinerary(title, imageSrc);
+
+            displayItinerary();
+            
+        }
+    });
+
+    // Get the current saved package container
+    const itineraryContainer = document.querySelector('.saved-package');
+
+    // Remove item from itinerary
+    itineraryContainer.addEventListener('click', (event) => {
+        //
+        if (event.target.classList.contains('btn-delete')) {
+            // Get the unique ID of the clicked item's parent
+            const itemId = event.target.closest('.package-elements').id;
+        
+            // Call the removeItemFromItinerary function with the item ID
+            removeItemFromItinerary(itemId);
+
+            displayItinerary();
+        }
+    });
+
+
+
+    document.getElementById('view-itin-btn').addEventListener('click', displayItinerary);
 
     
     populateCarouselImage();
@@ -40,13 +72,15 @@ async function populateCarouselImage() {
         carouselCaptionParent.setAttribute('class', 'carousel-caption d-none d-md-block');
 
         const carouselName = document.createElement('h3');
-        carouselName.setAttribute('class', 'text-uppercase');
+        carouselName.setAttribute('class', 'text-uppercase fw-bold text-start');
         carouselName.innerText = name;
 
         const carouselPlanet = document.createElement('h4');
+        carouselPlanet.setAttribute('class', 'text-start')
         carouselPlanet.innerText = planet;
 
         const carouselFact = document.createElement('p');
+        carouselFact.setAttribute('class', 'text-start')
         carouselFact.innerText = fact;
 
 
@@ -71,29 +105,8 @@ async function populateCarouselImage() {
 
     });
 
-    // console.log(sites);
+    
 }
 
 
-function addPackage() {
 
-}
-
-
-function removePackage() {
-
-}
-
-function displayItinerary() {
-
-}
-
-
-function generateTripSummary() {
-
-}
-
-
-function calculateTotalTripTime() {
-
-}
--- src/js/ItineraryService.js
+++ src/js/ItineraryService.js
@@ -1,11 +1,17 @@
 // Create an empty array of objects to store itinerary items 
-
 const itineraryItems = [];
+
+// Create a variable to item count
+let itemCount = 0;
 
 // Function for adding itinerary item to array
 export async function addToItinerary(title, imageSrc) {
+    // Increment itemcount 
+    itemCount++;
+
     // Create an object representing the itinerary item
     const newItem = {
+        id: `itineraryItem_${itemCount}`, // Unique ID 
         title: title,
         imageSrc: imageSrc
     };
@@ -14,9 +20,76 @@ export async function addToItinerary(title, imageSrc) {
     itineraryItems.push(newItem);
 
     // Update Itinerary UI in offcanvas
-    updateItineraryUI();
+    displayItinerary();
 }
 
-export function removeItemFromItinerary(title) {
-    
-}
+export function removeItemFromItinerary(itemId) {
+    // Find the index of the item with the given ID in the itineraryItems array
+    const indexToRemove = itineraryItems.findIndex((item) => item.id === itemId);
+  
+    if (indexToRemove !== -1) {
+      // Remove the item from the itineraryItems array
+      itineraryItems.splice(indexToRemove, 1);
+  
+      // After removing the item, reassign IDs to the remaining items
+      reassignItemIds();
+  
+      // You can also update local storage or a database to reflect the removal if needed
+
+    }
+  }
+
+// Function to reassign IDs to the remaining items
+function reassignItemIds() {
+    for (let i = 0; i < itineraryItems.length; i++) {
+      itineraryItems[i].id = `itineraryItem_${i + 1}`;
+    }
+  }
+
+
+  export function displayItinerary() {
+    const itineraryContainer = document.querySelector('.saved-package');
+    itineraryContainer.innerHTML = ''; // Clear existing content
+  
+    itineraryItems.forEach((item) => {
+        // Create a div for package items
+        const packageElement = document.createElement('div');
+        packageElement.setAttribute('class', 'package-elements');
+        packageElement.setAttribute('style', 'border-bottom: 1px solid; padding-bottom: 15px;');
+        packageElement.id = item.id;
+
+
+        // Create a heading element for the title
+        const titleElement = document.createElement('h1');
+        titleElement.setAttribute('class', 'modal-title fs-5 fw-bold package-label');
+        titleElement.setAttribute('style', 'text-align: left;')
+        titleElement.innerText = item.title;
+
+        // Create a div element to parent the image and button
+        const itinElements = document.createElement('div');
+        itinElements.setAttribute('class', 'itin-image-btn');
+  
+        // Create an image element
+        const imageElement = document.createElement('img');
+        imageElement.src = item.imageSrc;
+        imageElement.alt = item.title;
+        imageElement.setAttribute('class', 'offcanvas-img');
+  
+        // Create a button element to remove the item
+        const removeButton = document.createElement('button');
+        removeButton.setAttribute('class', 'btn-delete remove-itinerary-item');
+
+        // Append image and button to itinelements div
+        itinElements.appendChild(imageElement);
+        itinElements.appendChild(removeButton);
+  
+        // Append the image, title, and remove button to the package element
+        packageElement.appendChild(titleElement);
+        packageElement.appendChild(itinElements);
+  
+        // Append the itinerary item element to the container
+        itineraryContainer.appendChild(packageElement);
+    });
+  }
+  
+  
